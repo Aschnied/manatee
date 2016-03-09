a9c29619cf9372ddb6d926d785f8dae7aae0a71e
Andrew Schnieder <andrewschnieder@gmail.com>
2016-03-08 19:04:25 -0800

    Adding initial framework

---
+++ db/migrate/20160309003635_create_users.rb
@@ -0,0 +1,24 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.text :bio
+      t.integer :zip
+      t.boolean :javascript
+      t.boolean :ruby
+      t.boolean :php
+      t.boolean :html
+      t.boolean :css
+      t.boolean :rails
+      t.boolean :angular
+      t.boolean :node
+      t.boolean :bootstrap
+      t.boolean :sass
+      t.boolean :less
+      t.boolean :html5
+      t.boolean :sinatra
+
+      t.timestamps
+    end
+  end
+end
